642d95b6182a1fa46a40bb9255d1f67534333301
mrcyclo <vipboysanhdieu@gmail.com>
2026-04-28 00:26:06 +0700

    Update custom font size to 14px and integrate into project configuration

--- build_fonts.sh
+++ build_fonts.sh
@@ -6,6 +6,6 @@ lv_font_conv \
     -r 0x20-0x7F,0xA0-0x24F,0x1EA0-0x1EFF \
   --font "fonts/Font Awesome 7 Free-Solid-900.otf" \
     -r 0xF000-0xF8FF \
-  --size 16 --bpp 4 --format lvgl \
+  --size 14 --bpp 4 --format lvgl \
   -o src/font_custom_merged.c \
   --lv-font-name font_custom_merged
--- include/lv_conf.h
+++ include/lv_conf.h
@@ -708,10 +708,10 @@
  *  #define LV_FONT_CUSTOM_DECLARE   LV_FONT_DECLARE(my_font_1) LV_FONT_DECLARE(my_font_2)
  *  @endcode
  */
-#define LV_FONT_CUSTOM_DECLARE
+#define LV_FONT_CUSTOM_DECLARE LV_FONT_DECLARE(font_custom_merged)
 
 /** Always set a default font */
-#define LV_FONT_DEFAULT &lv_font_montserrat_14
+#define LV_FONT_DEFAULT &font_custom_merged
 
 /** Enable handling large font and/or fonts with a lot of characters.
  *  The limit depends on the font size, font face and bpp.
