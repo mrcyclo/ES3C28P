c3ce4de19c7f0f8752fced51714d1e07e4c4ea70
mrcyclo <vipboysanhdieu@gmail.com>
2026-04-28 00:09:24 +0700

    Add custom font generation script and integrate fonts into the project

---
+++ build_fonts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+lv_font_conv \
+  --no-compress \
+  --font fonts/arial.ttf \
+    -r 0x20-0x7F,0xA0-0x24F,0x1EA0-0x1EFF \
+  --font "fonts/Font Awesome 7 Free-Solid-900.otf" \
+    -r 0xF000-0xF8FF \
+  --size 16 --bpp 4 --format lvgl \
+  -o src/font_custom_merged.c \
+  --lv-font-name font_custom_merged
