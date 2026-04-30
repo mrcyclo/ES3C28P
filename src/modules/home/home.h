fd1ae8ea1a3087cf9da62640a98ddc6df95e1d3d
mrcyclo <vipboysanhdieu@gmail.com>
2026-05-01 03:35:28 +0700

    Refactor HomeClass UI layout and add drawer functionality for menu button

--- src/modules/home/home.h
+++ src/modules/home/home.h
@@ -9,6 +9,8 @@
 #include "msgbox/msgbox.h"
 #include "app_rainbow.h"
 
+#define MENU_BUTTON_SIZE 30
+
 class HomeClass : public ModuleOnce
 {
 public:
@@ -57,22 +59,45 @@ protected:
         lv_obj_set_size(image_wallpaper, lv_pct(100), lv_pct(100));
         lv_obj_add_flag(image_wallpaper, LV_OBJ_FLAG_HIDDEN);
 
-        auto button_menu = lv_btn_create(screen);
-        lv_obj_set_width(button_menu, lv_pct(95));
-        lv_obj_align(button_menu, LV_ALIGN_BOTTOM_MID, 0, -5);
+        auto content = lv_obj_create(screen);
+        lv_obj_align(content, LV_ALIGN_CENTER, 0, 0);
+        lv_obj_remove_flag(screen, LV_OBJ_FLAG_SCROLLABLE);
+        lv_obj_set_size(content, lv_pct(100), lv_pct(100));
+        lv_obj_set_layout(content, LV_LAYOUT_FLEX);
+        lv_obj_set_flex_flow(content, LV_FLEX_FLOW_COLUMN);
+        lv_obj_set_flex_align(content, LV_FLEX_ALIGN_END, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+        lv_obj_set_style_pad_all(content, 5, LV_PART_MAIN);
+        lv_obj_set_style_pad_top(content, STATUS_BAR_HEIGHT + 5, LV_PART_MAIN);
+        lv_obj_set_style_bg_opa(content, LV_OPA_0, LV_PART_MAIN);
+        lv_obj_set_style_border_width(content, 0, LV_PART_MAIN);
+
+        drawer = lv_obj_create(content);
+        lv_obj_set_width(drawer, lv_pct(100));
+        lv_obj_set_flex_grow(drawer, 1);
+        lv_obj_set_style_bg_color(drawer, lv_color_black(), LV_PART_MAIN);
+        lv_obj_set_style_bg_opa(drawer, LV_OPA_50, LV_PART_MAIN);
+        lv_obj_add_flag(drawer, LV_OBJ_FLAG_HIDDEN);
+
+        auto button_menu = lv_btn_create(content);
         lv_obj_add_event_cb(button_menu, LV_OBJ_EVENT_CB(HomeClass, button_menu_clicked_cb), LV_EVENT_CLICKED, this);
+        lv_obj_set_size(button_menu, MENU_BUTTON_SIZE, MENU_BUTTON_SIZE);
+        lv_obj_set_style_bg_color(button_menu, lv_color_black(), LV_PART_MAIN);
+        lv_obj_set_style_bg_opa(button_menu, LV_OPA_50, LV_PART_MAIN);
+        lv_obj_set_style_radius(button_menu, LV_RADIUS_CIRCLE, LV_PART_MAIN);
+        lv_obj_set_style_border_width(button_menu, 2, LV_PART_MAIN);
+        lv_obj_set_style_border_color(button_menu, lv_color_white(), LV_PART_MAIN);
 
         auto label_menu = lv_label_create(button_menu);
+        lv_label_set_text(label_menu, fa(0xf58d).c_str());
         lv_obj_align(label_menu, LV_ALIGN_CENTER, 0, 0);
-        lv_label_set_text(label_menu, "Applications");
 
-        auto sample_button = lv_button_create(screen);
-        lv_obj_align(sample_button, LV_ALIGN_CENTER, 0, 0);
-        lv_obj_add_event_cb(sample_button, LV_OBJ_EVENT_CB(AppRainbowClass, drawer_icon_clicked), LV_EVENT_CLICKED, static_cast<void *>(&AppRainbow));
-        AppRainbow.set_drawer_icon(sample_button);
-        auto sample_label = lv_label_create(sample_button);
-        lv_obj_align(sample_label, LV_ALIGN_CENTER, 0, 0);
-        lv_label_set_text(sample_label, AppRainbow.get_drawer_icon_text());
+        // auto sample_button = lv_button_create(screen);
+        // lv_obj_align(sample_button, LV_ALIGN_CENTER, 0, 0);
+        // lv_obj_add_event_cb(sample_button, LV_OBJ_EVENT_CB(AppRainbowClass, drawer_icon_clicked), LV_EVENT_CLICKED, static_cast<void *>(&AppRainbow));
+        // AppRainbow.set_drawer_icon(sample_button);
+        // auto sample_label = lv_label_create(sample_button);
+        // lv_obj_align(sample_label, LV_ALIGN_CENTER, 0, 0);
+        // lv_label_set_text(sample_label, AppRainbow.get_drawer_icon_text());
     }
 
 private:
@@ -81,11 +106,21 @@ private:
     bool loaded = false;
     lv_image_dsc_t wallpaper_dsc{};
     uint16_t *wallpaper_pixels = nullptr;
+    lv_obj_t *drawer = nullptr;
+    bool drawer_open = false;
 
     void button_menu_clicked_cb()
     {
-        Serial.println("[Home] Menu button clicked");
-        MsgBox.info("Applications drawer will be implemented soon!", nullptr, []() {});
+        if (drawer_open)
+        {
+            lv_obj_add_flag(drawer, LV_OBJ_FLAG_HIDDEN);
+            drawer_open = false;
+        }
+        else
+        {
+            lv_obj_remove_flag(drawer, LV_OBJ_FLAG_HIDDEN);
+            drawer_open = true;
+        }
     }
 };
 
